b193e7ce6def8a320431017dfb2ada2971fdbc2f
Gustav Tholén <tyontholen@gmail.com>
2025-09-20 18:06:49 +0200

    feat: initial commit, skeleton for weather app

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func usage() {
+	fmt.Println("Usage: weather <city>")
+}
+
+func main() {
+	if len(os.args) < 2 {
+		usage()
+		return
+	}
+
+	city := os.Args[1]
+	fmt.Println("You asked for the weather in:", city)
+
+	// TODO: make api call
+}
