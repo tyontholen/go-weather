d91c8ac050e023a7e32a4c6cb195fdc4f91551e5
Gustav Tholén <tyontholen@gmail.com>
2025-09-29 19:01:32 +0200

    feat: inputparsing

--- main.go
+++ main.go
@@ -3,11 +3,56 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
+	"weather/weather"
 )
 
-func usage() {
-	fmt.Println("Usage: weather <city>")
+var cityCoords = map[string][2][float64]{
+	"london": {51.5072, -0.1276},
+	"stockholm": {59.3293, 18.0686},
+	"newyork": {40.7128, -74.0060},
+	"tokyo": {35.6762, 139.6503},
+	"oslo": {59.9139, 10.7522},
 }
+
+func usage() {
+	fmt.Println("Usage:")
+	fmt.Println(" weather <city>			(e.g weather Stockholm)")
+	fmt.Println(" weather <latitude> <lon>	(e.g weather 59.3293 18.0686)")
+}
+
+func parseInputs(args []string) (float64, float64, error) {
+	// if single argument, try city lookup
+
+	if len(args) == 1 {
+		city := strings.ToLower(args[0])
+		if coords, ok := cityCoords[city]; ok {
+			return coords[0], coords[1], nil
+		}
+		return 0, 0, fmt.Errorf("Error: Unknown city: %s", args[0])
+	}
+	
+	// if two or more args, try parse the first 2 as latitude & longitude
+	if len(args) >= 2 {
+		lat, err1 := strconv.ParseFloat(args[0], 64)
+		lon, err2 := strconv.ParseFloat(args[1], 64)
+		if err == nil && err2 == nil {
+			return lat, lon, nil
+		}
+	
+	// if parsing fails, maybe user types multi-word city, like New York
+	city := strings.ToLower(strings.Join(args, ""))
+	if coords, ok := cityCoords[city]; ok {
+		return coords[0], coords[1], nil
+	}
+	return 0, 0, fmt.Errorf("Error: Could njot parse coordinates or find city")
+
+	}
+	return 0, 0, fmt.Errorf("Error: Not enough arguments")
+}
+
+
 
 func main() {
 	if len(os.Args) < 2 {
